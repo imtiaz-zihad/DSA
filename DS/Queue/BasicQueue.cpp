d0b40a48ef892ea35f46db7e4345229beba9549d
Imtiaz <mdimtiazzihad@gmail.com>
2025-07-22 19:11:53 +0600

    Basic Queue & Queue using singly

---
+++ DS/Queue/BasicQueue.cpp
@@ -0,0 +1,21 @@
+/**
+Queue Use FIFO (First In,First Out) Method 
+ 
+Queue Is a --> Abstract & Higher Level Data Structer 
+
+Queue is similar STACK it value PUSH in last But 
+main diffarence is POP it  remove value from First
+  
+Queue (FIFO)
+
+Push → Add at End  
+Pop  → Remove from Front
+
+Push 10 → [10]  
+Push 20 → [10, 20]  
+Push 30 → [10, 20, 30]  
+
+Pop     → [20, 30]     (10 removed)  
+Pop     → [30]         (20 removed)  
+
+ */
---
+++ DS/Queue/SinglyQueue.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+class Node{
+public:
+  int val;
+  Node* next;
+  Node(int val){
+    this->val= val;
+    this->next= NULL;
+  }
+};
+
+class myQueue{
+public:
+  Node* head = NULL;
+  Node* tail = NULL;
+
+  int sz= 0;
+
+  void push(int val){ // O(1)
+    Node* newnode = new Node(val);
+    sz++;
+
+    if(head==NULL){//corner Case
+      head = newnode;
+      tail = newnode;
+
+      return;
+    }
+
+    tail->next = newnode;
+    tail = newnode;
+  }
+
+  void pop(){ //O(1)
+    sz--;
+    Node* deleteNode = head;
+    head= head->next;
+    delete deleteNode;
+
+    if(head == NULL){ //corner Case
+      tail=NULL;
+    };
+  }
+  int front(){ // O(1)
+    return head->val;
+  }
+  int back(){ // O(1)
+    return tail->val;
+  }
+  int size(){
+    return sz;
+  }
+  bool empty(){
+    return head == NULL;
+  }
+
+};
+
+int solve() {
+
+  myQueue q;;
+  
+
+  int n ;
+  cin >> n;
+
+  for(int i=0;i<n;i++){
+    int x;
+    cin >> x;
+    q.push(x);
+  }
+
+
+  while(!q.empty()){
+    cout << q.front() << endl;
+    q.pop(); 
+  }
+
+
+
+   return 0;
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
+ 
