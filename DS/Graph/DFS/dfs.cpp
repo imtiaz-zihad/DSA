21ca03cbaae844423d10e8f587c8636089a3e4a0
Imtiaz <mdimtiazzihad@gmail.com>
2025-09-03 23:04:11 +0600

    Add DFS implementation for depth-first graph traversal

---
+++ DS/Graph/DFS/dfs.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+vector<int> adj_list[1005];
+bool vis[1005];
+
+void dfs(int src)
+{
+    cout << src << " ";
+    vis[src] = true;
+
+    for (int child : adj_list[src])
+    {
+        if (!vis[child])
+            dfs(child);
+    }
+}
+
+int solve()
+{
+    int n, m;
+    cin >> n >> m;
+    while (m--)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u); // for undirected graph
+    }
+
+    memset(vis, false, sizeof(vis));
+    dfs(0);
+
+    return 0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
