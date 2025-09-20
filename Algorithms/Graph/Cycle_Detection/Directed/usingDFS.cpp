3ae28148f8ccaab5c20fbb8c55478f375fd44fc6
Imtiaz <mdimtiazzihad@gmail.com>
2025-09-20 15:56:20 +0600

    Update file Structer

---
+++ Algorithms/Graph/Cycle_Detection/Directed/usingDFS.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool vis[105];
+bool pathvis[105];
+bool cycle;
+vector<int>adj_list[105];
+    
+void dfs(int src){
+  vis[src] = true;
+  pathvis[src] = true;
+  
+  for(int child: adj_list[src]){
+    if(vis[child] && pathvis[child])
+    cycle = true;
+    if(!vis[child]){
+      dfs(child);
+    }
+  }
+  // return when no circle found 
+  pathvis[src] = false;
+}
+
+int main() 
+{
+    int n,m;
+    cin >> n >> m;
+
+    while(m--){
+      int a,b;
+      cin >> a >> b;
+      adj_list[a].push_back(b);
+    }
+    memset(vis,false,sizeof(vis));
+    memset(pathvis,false,sizeof(pathvis));
+    cycle = false;
+    for(int i=0;i<n;i++){
+      if(!vis[i])
+      dfs(i);
+    }
+    if(cycle)
+    cout << "Cycle Detected";
+    else 
+    cout << "NO cycle Detected";
+    return 0;
+}
